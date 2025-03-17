af09cbedff9ea2cc4772af56e68ac27a26b49182
Pankaj Verma <pankajv91505@gmail.com>
2025-03-17 23:18:56 +0530

    Refactor backend database connection and update product data access methods; remove unused background image and clean up CSS

--- Backend/uom_dao.py
+++ Backend/uom_dao.py
@@ -1,19 +1,19 @@
-
-
-def get_uoms(connection): 
-    
+def get_uoms(connection):
     cursor = connection.cursor()
-    cursor.execute("query")
-    
-    respose = []
+    query = ("select * from uom")
+    cursor.execute(query)
+    response = []
     for (uom_id, uom_name) in cursor:
-        respose.append({
-            "uom_id": uom_id,
-            "uom_name": uom_name
+        response.append({
+            'uom_id': uom_id,
+            'uom_name': uom_name
         })
-    return respose
+    return response
+
 
 if __name__ == '__main__':
-    from mysql_connection import get_sql_connection
+    from Grocery_Store_System.Backend.sql_connection import get_sql_connection
+
     connection = get_sql_connection()
+    # print(get_all_products(connection))
     print(get_uoms(connection))
